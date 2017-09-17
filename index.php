8db0cb6654656ff7c2ed91648caddd10df6848fd
Volksein <ale@volksein.com.br>
2017-09-17 17:29:56 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 header('Content-Type: text/html; charset=UTF-8');
 include('correios.class.php');
 if(isset($_GET['cep'])){
-	die(json_encode(Correios::cep($_GET['cep'])));
+	die(json_encode(Correios::cep($_GET['cep']), JSON_UNESCAPED_UNICODE));
 }elseif(isset($_GET['codigo_rastreio'])){
-	die(json_encode(Correios::rastreio($_GET['codigo_rastreio'])));
+	die(json_encode(Correios::rastreio($_GET['codigo_rastreio']), JSON_UNESCAPED_UNICODE));
 }else{
 	die('informe parametro GET cep ou codigo_rastreio');
 }
